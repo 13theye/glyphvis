9eb0b26d398827eaa771e0f8a96a8d14e2697bbc
13theye <jjhh@13theye.com>
2025-02-04 00:47:29 +0900

    Removed SegmentEffect Trait

--- src/effects/effects_manager.rs
+++ src/effects/effects_manager.rs
@@ -5,38 +5,12 @@ use std::collections::HashMap;
 
 pub enum EffectType {
     Grid(Box<dyn Effect>),
-    Segment(Box<dyn SegmentEffect>),
 }
 
 // the base Effect trait which all effects must implement
 pub trait Effect {
     fn apply(&self, style: &DrawStyle, time: f32) -> DrawStyle;
     fn is_finished(&self) -> bool;
-}
-
-pub trait SegmentEffect: Effect {
-    fn apply_to_segment(
-        &self,
-        segment_id: &str,
-        base_style: &DrawStyle,
-        target_style: &DrawStyle,
-        current_time: f32,
-    ) -> DrawStyle;
-    fn activate_segment(&mut self, segment_id: &str, time: f32);
-    fn deactivate_segment(&mut self, segment_id: &str);
-    fn is_segment_active(&self, segment_id: &str) -> bool;
-    fn is_effect_finished(&self) -> bool;
-}
-
-impl<T: SegmentEffect> Effect for T {
-    // This won't be used for segment effects
-    fn apply(&self, style: &DrawStyle, _time: f32) -> DrawStyle {
-        style.clone()
-    }
-
-    fn is_finished(&self) -> bool {
-        SegmentEffect::is_effect_finished(self)
-    }
 }
 
 // Effect instance with metadata
@@ -86,44 +60,6 @@ impl EffectsManager {
         }
     }
 
-    /* Deprecating
-    // Apply all active effects
-    pub fn apply_segment_effects(
-        &self,
-        segment_id: &str,
-        base_style: DrawStyle,
-        target_style: DrawStyle,
-        time: f32,
-    ) -> DrawStyle {
-        if self.effects.is_empty() {
-            return base_style;
-        }
-
-        let mut current_style = base_style.clone();
-
-        for instance in self.effects.values() {
-            if !instance.is_active {
-                continue;
-            }
-
-            match &instance.effect {
-                EffectType::Grid(_) => {
-                    //current_style = effect.apply(&base_style, time);
-                    continue;
-                }
-                EffectType::Segment(effect) => {
-                    if effect.is_segment_active(segment_id) {
-                        current_style =
-                            effect.apply_to_segment(segment_id, &base_style, &target_style, time);
-                    }
-                }
-            }
-        }
-
-        current_style
-    }
-     */
-
     // Apply all active effects
     pub fn apply_grid_effects(&self, base_style: DrawStyle, time: f32) -> DrawStyle {
         if self.effects.is_empty() {
@@ -141,31 +77,16 @@ impl EffectsManager {
                 EffectType::Grid(effect) => {
                     current_style = effect.apply(&current_style, time);
                 }
-                EffectType::Segment(_effect) => {
-                    continue;
-                }
             }
         }
 
         current_style
     }
 
-    /* Deprecating
-    // for segment-specific operations
-    pub fn activate_segment(&mut self, segment_id: &str, effect_name: &str, time: f32) {
-        if let Some(instance) = self.effects.get_mut(effect_name) {
-            if let EffectType::Segment(effect) = &mut instance.effect {
-                effect.activate_segment(segment_id, time);
-            }
-        }
-    }
-    */
-
     // Clean up finished effects
     pub fn cleanup(&mut self) {
         self.effects.retain(|_, instance| match &instance.effect {
             EffectType::Grid(effect) => !effect.is_finished(),
-            EffectType::Segment(effect) => !effect.is_finished(),
         });
     }
 

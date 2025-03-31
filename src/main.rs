745dac0bfa35df6f92db05b1081f9f16e4dee8bc
13theye <jjhh@13theye.com>
2025-03-31 15:34:23 +0900

    bumped up recording quality

--- src/main.rs
+++ src/main.rs
@@ -104,7 +104,7 @@ fn model(app: &App) -> Model {
     // Create window
     let window_id = app
         .new_window()
-        .title("glyphvis 0.3.2")
+        .title("glyphvis 0.3.2b")
         .size(config.window.width, config.window.height)
         .msaa_samples(1)
         .view(view)
